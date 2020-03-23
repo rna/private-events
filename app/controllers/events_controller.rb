87624391402cee386b7c6ddb33733eef8a4cb082
Ramesh <rameshnaidu.allu@gmail.com>
2020-03-24 02:49:19 +0530

    add new event and index pages

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,32 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @user = User.find_by(id:session[:user_id])
+    @event = @user.events.build
+  end
+
+  def create
+    @user = User.find_by(id: session[:user_id])
+    @event = @user.events.build(user_params)
+    if @event.save
+      flash[:success] = "Event created successfully"
+      render 'show'
+    else
+      flash.now[:danger] = "Invalid Event"
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:event).permit(:title, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   root to: 'sessions#new'
 
   get '/signup', to: 'users#new'
-  # get '/profile', to: 'users#show:id'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
   resources :users, only: %i[new create show]
+  resources :events, only: %i[index new create show]
+
 end
