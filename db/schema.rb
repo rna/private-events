7cfe14fd4c76e943e3486738a3ae00594a4f9e20
Ramesh <rameshnaidu.allu@gmail.com>
2020-03-24 00:20:40 +0530

    add event model and association to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_180658) do
+ActiveRecord::Schema.define(version: 2020_03_23_060311) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
